5effa564632b013f0cc38b466d8d3568eb9fb834
s-raza+S20-Win <12784652+S20-Win+s-raza@users.noreply.github.com>
2021-09-19 23:28:56 +0400

    Implement CSVBase

---
+++ csvio/csvbase.py
@@ -0,0 +1,19 @@
+from typing import Any, Dict, List
+
+from .filebase import FileBase
+
+
+class CSVBase(FileBase):
+    def __init__(self, filename: str) -> None:
+        super().__init__(filename)
+
+        self._fieldnames: List[str] = []
+        self.rows: List[Dict[str, Any]] = []
+
+    @property
+    def fieldnames(self) -> List[str]:
+        return list(self._fieldnames)
+
+    @fieldnames.setter
+    def fieldnames(self, fieldnames: List[str]) -> None:
+        self._fieldnames = fieldnames
