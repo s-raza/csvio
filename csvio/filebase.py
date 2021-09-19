09093038989e91f61e2de0c26676f6cfd1f0fcd6
s-raza+S20-Win <12784652+S20-Win+s-raza@users.noreply.github.com>
2021-09-19 14:28:03 +0400

    Refactor to use pathlib library instead of os+
    Update tests

--- csvio/filebase.py
+++ csvio/filebase.py
@@ -1,9 +1,14 @@
-import os
+from pathlib import Path
 
 
 class FileBase:
     def __init__(self, file_name: str) -> None:
 
-        self.filename = os.path.basename(file_name)
-        self.filedir = os.path.dirname(file_name)
-        self.filepath_no_ext, self.file_ext = os.path.splitext(file_name)
+        self.path_obj = Path(file_name)
+
+        self.filename = self.path_obj.name
+        self.filedir = str(self.path_obj.parent)
+        self.filepath = str(Path().joinpath(self.filedir, self.filename))
+
+        self.filename_no_ext = self.path_obj.stem
+        self.file_ext = self.path_obj.suffix
--- tests/test_filebase.py
+++ tests/test_filebase.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from csvio.filebase import FileBase
 
 
@@ -6,8 +8,9 @@ def test_normal():
     fb = FileBase("/a/b/c.csv")
 
     assert fb.filename == "c.csv"
-    assert fb.filedir == "/a/b"
-    assert fb.filepath_no_ext == "/a/b/c"
+    assert fb.filedir == str(Path("/a/b"))
+    assert fb.filepath == str(Path("/a/b/c.csv"))
+    assert fb.filename_no_ext == "c"
     assert fb.file_ext == ".csv"
 
 
@@ -16,6 +19,7 @@ def test_filename_no_base_path():
     fb = FileBase("c.csv")
 
     assert fb.filename == "c.csv"
-    assert fb.filedir == ""
-    assert fb.filepath_no_ext == "c"
+    assert fb.filedir == "."
+    assert fb.filepath == "c.csv"
+    assert fb.filename_no_ext == "c"
     assert fb.file_ext == ".csv"
