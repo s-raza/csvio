2a8f48edb38685615be744501b3d4ddf050eca32
s-raza+S20-Win <12784652+S20-Win+s-raza@users.noreply.github.com>
2021-10-03 12:40:42 +0400

    Implement rows_to_nested_dicts method in CSVBase

--- csvio/csvbase.py
+++ csvio/csvbase.py
@@ -93,3 +93,38 @@ class CSVBase(FileBase):
 
             if arg not in dict_to_update:
                 dict_to_update[arg] = value
+
+    def rows_from_column_key(
+        self, column_name: str, rows: List[Dict[str, Any]] = None
+    ) -> Dict[str, List[Dict[str, Any]]]:
+
+        ret_dict: Dict[str, Any] = {}
+        rows = rows or self.rows
+
+        for row in rows:
+            ret_dict.setdefault(row[column_name], []).append(row)
+
+        return ret_dict
+
+    def rows_to_nested_dicts(
+        self, column_order: List[str], rows: List[Dict[str, Any]] = None
+    ) -> Dict[str, Any]:
+
+        rows = rows or self.rows
+
+        if len(column_order) == 1:
+            return self.rows_from_column_key(column_order[0], rows)
+
+        ret_dict: Dict[str, Any] = {}
+
+        for row in self.rows:
+
+            temp_dict = ret_dict.setdefault(row[column_order[0]], {})
+
+            for column in column_order[1:-1]:
+
+                temp_dict = temp_dict.setdefault(row[column], {})
+
+            temp_dict.setdefault(row[column_order[-1]], []).append(row)
+
+        return ret_dict
