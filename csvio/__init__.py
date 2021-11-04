b2a4e011ec8b76461811333d4809a7ea47416b29
s-raza+S20-Win <12784652+S20-Win+s-raza@users.noreply.github.com>
2021-11-04 19:30:38 +0400

    Bump version to 0.4.0

--- csvio/__init__.py
+++ csvio/__init__.py
@@ -21,6 +21,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 from .csvreader import CSVReader
 from .csvwriter import CSVWriter
