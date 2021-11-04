3ad047fb745a616e9aeff2c065ff10cb2033d72a
s-raza+S20-Win <12784652+S20-Win+s-raza@users.noreply.github.com>
2021-11-04 23:03:21 +0400

    Implement process_row for RowProcessor, write test

--- csvio/processors/row_processor.py
+++ csvio/processors/row_processor.py
@@ -14,14 +14,34 @@ class RowProcessor(ProcessorBase):
 
         super().__init__(handle)
 
+    def add_processor_handle(self, handle: str) -> None:
+        ProcessorBase.processors[handle] = []
+
     def add_processor(
         self, func_: Union[List[RP], RP], handle: str = None
     ) -> None:
 
-        pass
+        handle = handle or self.handle
+
+        if not isinstance(func_, list):
+            func_ = [func_]
+
+        ProcessorBase.processors[handle].extend(func_)
 
     def process_row(
         self, row: R, processor_handle: Union[Type[ProcessorBase], str] = None
     ) -> R:
 
-        pass
+        applied_handle = processor_handle or self.handle
+
+        if isinstance(applied_handle, ProcessorBase):
+            applied_handle = applied_handle.handle
+
+        processors = ProcessorBase.processors[str(applied_handle)]
+
+        temp_row = row
+
+        for processor_func in processors:
+            temp_row = processor_func(temp_row)
+
+        return temp_row
---
+++ tests/test_row_processor.py
@@ -0,0 +1,88 @@
+from csvio.processors import RowProcessor
+
+from .csv_data import get_csv_reader_writer
+
+result = [
+    {
+        "Supplier": "Big Apples (Spain)",
+        "Fruit": "Apple",
+        "Origin": "Spain",
+        "Quantity": 1,
+    },
+    {
+        "Supplier": "Big Melons (Italy)",
+        "Fruit": "Melons",
+        "Origin": "Italy",
+        "Quantity": 2,
+    },
+    {
+        "Supplier": "Long Mangoes (India)",
+        "Fruit": "Mango",
+        "Origin": "India",
+        "Quantity": 3,
+    },
+    {
+        "Supplier": "Small Strawberries (France)",
+        "Fruit": "Strawberry",
+        "Origin": "France",
+        "Quantity": 4,
+    },
+    {
+        "Supplier": "Short Mangoes (France)",
+        "Fruit": "Mango",
+        "Origin": "France",
+        "Quantity": 5,
+    },
+    {
+        "Supplier": "Sweet Strawberries (Spain)",
+        "Fruit": "Strawberry",
+        "Origin": "Spain",
+        "Quantity": 7,
+    },
+    {
+        "Supplier": "Square Apples (Italy)",
+        "Fruit": "Apple",
+        "Origin": "Italy",
+        "Quantity": 8,
+    },
+    {
+        "Supplier": "Small Melons (Italy)",
+        "Fruit": "Melons",
+        "Origin": "Italy",
+        "Quantity": 9,
+    },
+    {
+        "Supplier": "Dark Berries (Australia)",
+        "Fruit": "Strawberry",
+        "Origin": "Australia",
+        "Quantity": 10,
+    },
+    {
+        "Supplier": "Sweet Berries (Australia)",
+        "Fruit": "Blackcurrant",
+        "Origin": "Australia",
+        "Quantity": 11,
+    },
+]
+
+
+def update_row(row):
+
+    row["Supplier"] = f"{row['Supplier']} ({row['Origin']})"
+
+    row["Quantity"] = int(row["Quantity"])
+
+    if row["Quantity"] > 5:
+        row["Quantity"] += 1
+
+    return row
+
+
+def test_row_processor(tmp_path):
+
+    _, reader = get_csv_reader_writer(tmp_path)
+
+    rproc = RowProcessor("rp1")
+    rproc.add_processor(update_row)
+
+    assert rproc.process_rows(reader.rows) == result
