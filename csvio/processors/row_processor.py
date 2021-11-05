2382c3ec5deabb46f107f27d21590e3181cd753e
s-raza+S20-Win <12784652+S20-Win+s-raza@users.noreply.github.com>
2021-11-05 13:31:31 +0400

    Add documentation for RowProcessor

--- csvio/processors/row_processor.py
+++ csvio/processors/row_processor.py
@@ -6,7 +6,7 @@ from .processor_base import ProcessorBase
 
 class RowProcessor(ProcessorBase):
     """
-    :param handle: Reference handle for the field processor
+    :param handle: Reference handle for the row processor
     :type handle: required
     """
 
@@ -20,6 +20,34 @@ class RowProcessor(ProcessorBase):
     def add_processor(
         self, func_: Union[List[RP], RP], handle: str = None
     ) -> None:
+        """
+        Add a processor function to process rows.
+
+        The processor function reference is essentially a callback function
+        that accepts a single argument that represents a row.
+
+        The ``fieldnames`` from a CSV are used within this callback function as
+        the keys to this single argument that represents a row to access its
+        values and perform the required transformations. This callback function
+        should return a dictionary representing a row, once all the required
+        transformations are applied.
+
+        :param func_: Row processor callback function reference or a list of such
+            function references.
+            All function references added with the same handle will be executed
+            for the row, to transform its value in the same order as they are
+            added. Usually a single processor function maybe sufficient to
+            perform all the transformations that are required for a row.
+        :type func_: required
+
+        :param handle: Processor reference handle to which the processor will
+            be added.
+            If not provided, the handle of the current object will be used.
+        :type handle: optional
+
+        :return: None
+
+        """
 
         handle = handle or self.handle
 
@@ -31,6 +59,34 @@ class RowProcessor(ProcessorBase):
     def process_row(
         self, row: R, processor_handle: Union[Type[ProcessorBase], str] = None
     ) -> R:
+        """
+        Process a single row.
+
+        This applies the processors defined using the
+        :py:func:`~csvio.processors.row_processor.RowProcessor.add_processor`
+        function in the same order that they were added using
+        :py:func:`~csvio.processors.row_processor.RowProcessor.add_processor`
+
+        The output row after application of the previous processor function
+        is passed on to the next processor function that was added using
+        :py:func:`~csvio.processors.row_processor.RowProcessor.add_processor`,
+        and the output of the last processor function added is returned as the
+        final output of this function.
+
+        :param row: A single dictionary of ``fieldname->value`` pairs
+            representing a single row
+        :type row: required
+
+        :param processor_handle: A processor handle or an object that
+            references the processor functions to apply and transform the row
+            values. The processor functions of the current object are used if
+            this argument is not provided.
+
+        :type processor_handle: optional
+
+        :return: A dictionary representing a processed CSV row
+
+        """
 
         applied_handle = processor_handle or self.handle
 
---
+++ docs/source/csvio.rowprocessor.rst
@@ -0,0 +1,16 @@
+Row Processor
+================
+
+A Row Processor may be used to transform the values of a row represented by
+a dictionary that maps ``column->value`` pairs. This processor can be used in
+situations where you need to transform values of particular fields in a row
+depending upon the values of some other fields within the same row.
+
+In *csvio* a CSV file is represented by a list of dictionaries that is
+populated in the ``rows`` attribute of the :py:class:`~csvio.CSVReader` or
+:py:class:`~csvio.CSVWriter` Classes.
+
+.. autoclass:: csvio.processors.row_processor.RowProcessor
+    :show-inheritance:
+    :members:
+    :inherited-members:
