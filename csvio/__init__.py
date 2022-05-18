6309fbfe02fcd95e9c45b5c8b33bb4ec998c82e5
s-raza+S20-Win <12784652+S20-Win+s-raza@users.noreply.github.com>
2022-05-18 10:18:14 +0400

    Bump patch version to v1.1.1

--- csvio/__init__.py
+++ csvio/__init__.py
@@ -21,6 +21,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 from .csvreader import CSVReader
 from .csvwriter import CSVWriter
